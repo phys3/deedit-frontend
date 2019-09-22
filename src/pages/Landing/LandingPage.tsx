d675164dcfb1d4444492957494a0d889b8ec1b8e
Filip Strelec <filip.strelec@speck.agency>
2019-09-22 17:30:21 +0200

    In dev

---
+++ src/pages/Landing/LandingPage.tsx
@@ -0,0 +1,21 @@
+
+import * as React from 'react';
+import {Link} from "react-router-dom";
+import {Page, PageDocumentHead, PageHeader} from "../Components/index";
+
+
+const LandingPage =()=>{
+
+return (
+<Page>
+    <PageDocumentHead/>
+    <PageHeader/>
+    <Link to="/post">Landing page</Link>
+</Page>
+
+);
+
+
+};
+
+export default LandingPage;
