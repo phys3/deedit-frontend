c5907ccd945f92bc369f4f3a0a2f55f943a1c5f7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-04 20:06:24 +0000

    Deploy phys3/deedit-frontend to github.com/phys3/deedit-frontend.git:gh-pages

---
+++ precache-manifest.35ca142a84b02726931276763b941acf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feacb0167ea084c076e62b3124faa271",
+    "url": "/deedit-frontend/index.html"
+  },
+  {
+    "revision": "5060d10e41877c6895e6",
+    "url": "/deedit-frontend/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "e6f9b88f7bad1967a319",
+    "url": "/deedit-frontend/static/js/2.16acb4d3.chunk.js"
+  },
+  {
+    "revision": "5060d10e41877c6895e6",
+    "url": "/deedit-frontend/static/js/main.1f13ffd1.chunk.js"
+  },
+  {
+    "revision": "7912bbcf8fbcc7e9ce2d",
+    "url": "/deedit-frontend/static/js/runtime~main.cb202c41.js"
+  }
+]);
